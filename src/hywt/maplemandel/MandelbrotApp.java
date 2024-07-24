c05487f0acf90230971897e14f31991ade107d72
haveyouwantto <haveyouwantto@gmail.com>
2024-07-24 13:03:23 +0800

    4.4 commit

--- src/hywt/maplemandel/MandelbrotApp.java
+++ src/hywt/maplemandel/MandelbrotApp.java
@@ -1,9 +1,11 @@
 package hywt.maplemandel;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MandelbrotApp {
     public static void main(String[] args) {
@@ -19,6 +21,34 @@ public class MandelbrotApp {
 
         // 显示窗口
         frame.setVisible(true);
+
+
+        // 创建工具栏并添加保存按钮
+        JToolBar toolBar = new JToolBar();
+        JButton saveButton = new JButton("保存");
+
+        saveButton.addActionListener(e -> {
+            // 弹出文件选择对话框
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("保存图像");
+
+            int userSelection = fileChooser.showSaveDialog(null);
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File fileToSave = fileChooser.getSelectedFile();
+                if (!fileToSave.getName().endsWith(".png")) fileToSave = new File(fileToSave.getAbsolutePath()+".png");
+                try {
+                    // 保存图像为PNG文件
+                    ImageIO.write(panel.getImage(), "png", fileToSave);
+                    JOptionPane.showMessageDialog(null, "图像已保存为 " + fileToSave.getAbsolutePath());
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "保存图像时出错！");
+                }
+            }
+        });
+
+        toolBar.add(saveButton);
+        frame.getContentPane().add(toolBar,BorderLayout.NORTH);
     }
 }
 
@@ -57,5 +87,9 @@ class DrawingPanel extends JPanel {
 
             g.drawImage(image, x, y, newWidth, newHeight, null);
         }
+    }
+
+    public BufferedImage getImage() {
+        return image;
     }
 }
