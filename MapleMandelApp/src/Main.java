0f4a4e976401333c28155faeb2a6a60bddacf543
haveyouwantto <haveyouwantto@gmail.com>
2024-09-16 05:34:59 +0800

    fix floatexp edge cases

--- MapleMandelApp/src/Main.java
+++ MapleMandelApp/src/Main.java
@@ -3,6 +3,9 @@ import hywt.maplemandel.ui.MandelbrotApp;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        FloatExp exp = FloatExp.parseFloatExp("123e232");
+        FloatExp exp2 = FloatExp.parseFloatExp("1323e232");
+        System.out.println(exp.mul(exp2));
         MandelbrotApp app = new MandelbrotApp();
     }
 }
